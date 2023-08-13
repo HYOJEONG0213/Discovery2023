3db7d01b76b17d472035cdf92849dfdd9173a7ed
HYOJEONG0213 <88478785+HYOJEONG0213@users.noreply.github.com>
2023-08-13 17:55:45 +0900

    양효정 - 블록 배치, 빼기(버그 있음)

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ from blockset_ai import *
 board = [[1,0,0,0,0,0,1],
         [1,0,0,0,0,0,1],
         [1,1,0,0,1,1,1]]
-block = [(0,0), (0,1), (-1,1)]
+block_list = [[(0,0), (0,1), (-1,1)], [(0,0), (0,1), (0,2)], [(0,0), (0,1), (0,2), (1,0)], [(0,0), (0,1), (0,2), (1,1)], [(0,0), (0,1), (1,0), (1,1)]]
+block = []
+#block = [(0,0), (0,1), (-1,1)]
 '''block = [[(0,0), (0,1), (-1,1)], [(0,0),(1,0),(1,1)], [(0,0),(1,0),(0,1)], [(0,0),(0,1),(1,1)], #ㄴ 모양
          [(0,0), (0,1), (0,2)], [(0,0), (1,0), (2,0)], #일렬, 3칸
          [(0,0), (0,1), (0,2), (1,0)], [(0,0), (0,1), (0,2), (-1,0)], [(0,0), (0,1), (0,2), (1,2)], [(0,0), (0,1), (0,2), (-1,2)], [(0,0), (1,0), (2,0), (0,1)], [(0,0), (1,0), (2,0), (0,-1)], [(0,0), (1,0), (2,0), (2,1)], [(0,0), (1,0), (2,0), (2,-1)], #긴 ㄴ모양
@@ -23,10 +25,16 @@ pygame.init()
 
 H, W = 3, 7
 block_num = 2
+selected_color_num = None
 original_board = [row[:] for row in board]  # 게임 보드의 초기 상태 저장
+original_block = [row[:] for row in block_list]
 
-screen = pygame.display.set_mode((W * 50, H * 50))
+screen = pygame.display.set_mode((W * 50, H * 200))
 pygame.display.set_caption("Block Game")
+
+font = pygame.font.Font('Maplestory Bold.ttf', 36)  # 폰트 설정
+message = ""
+message_pos = (60, 500)  # 메시지 위치 설정 (x, y)
 
 def check_block_placement(mouse_x, mouse_y, block_x, block_y):  #해당 위치에 블록을 넣을 수 있는지 판별
     for dx, dy in block:
@@ -45,10 +53,10 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        elif event.type == pygame.MOUSEMOTION:  #마우스 이동 이벤트 발생시 마우스 x, y값 가져옴
+        elif block!=None and event.type == pygame.MOUSEMOTION:  #마우스 이동 이벤트 발생시 마우스 x, y값 가져옴
             mouse_x, mouse_y = event.pos
             block_x = mouse_x // 50
-            block_y = mouse_y // 50
+            block_y = mouse_y // 60
             
             if hovered_block == None:
                 if check_block_placement(mouse_x, mouse_y, block_x, block_y):   #블록을 현재 위치에 넣을 수 있다면 확인하기
@@ -78,18 +86,18 @@ while running:
                 else:
                     hovered_block = None
 
-        elif event.type == pygame.MOUSEBUTTONDOWN:    #마우스 클릭시
+        elif block!=None and event.type == pygame.MOUSEBUTTONDOWN:    #마우스 클릭시
             if color_changed and hovered_block:     #색상이 변경된 상태이면서 hover한 블록이 있는 경우
                 for dx, dy in block:
                     nx, ny = hovered_block[0] + dx, hovered_block[1] + dy
                     if 0 <= nx < W and 0 <= ny < H:
                         board[ny][nx] = original_board[ny][nx]  #원래 상태로 되돌리기
-                color_changed = False   #색상 변경 상태 초기화
+                color_changed = False   #색상 변경 상태 초기화 
                 hovered_block = None
             else:
                 mouse_x, mouse_y = event.pos
                 block_x = mouse_x // 50
-                block_y = mouse_y // 50
+                block_y = mouse_y // 60
                 if check_block_placement(mouse_x, mouse_y, block_x, block_y):
                     for dx, dy in block:
                         nx, ny = block_x + dx, block_y + dy
@@ -97,10 +105,64 @@ while running:
                             original_board[ny][nx] = board[ny][nx]
                             board[ny][nx] = block_num
                     color_changed = True
+                    block = []
+                    message = ""
+                    block_list.remove(block_list[select_block_num])
                     block_num += 1
+                    '''for block_info in block_info:
+                        if block_info["color_num"] == selected_color_num:
+                            block_list'''
                     
 
+        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 3:  # 오른쪽 마우스 버튼 클릭 이벤트일 때
+            mouse_x, mouse_y = event.pos
+            block_x = mouse_x // 50
+            block_y = mouse_y // 50
+            a = []
+            if 0 <= block_x < W and 0 <= block_y < H:
+                clicked_color_num = board[block_y][block_x]
+                if clicked_color_num != 0:  # 클릭한 위치에 색상이 있는 경우
+                    for y in range(H):
+                        for x in range(W):
+                            if board[y][x] == clicked_color_num:
+                                board[y][x] = 0
+                                a.append((y, x))
+                    block_num -=1
+            block_list.append(a)
+
+
+
+                    
+
+
     draw_board(screen, board)
+    block_info = draw_block(screen, block_list, H, block_num)
+
+    
+
+    if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:  # 마우스 왼쪽 버튼 클릭 이벤트일 때
+        mouse_x, mouse_y = event.pos
+        selected_color_num = None
+        for block_info in block_info:
+            if block_info["rect"].collidepoint(mouse_x, mouse_y):
+                selected_color_num = block_info["color_num"]
+                break
+                # selected_color_num에 해당하는 블록들을 찾아 처리
+                #for block_info in block_info:
+                    #if block_info["color_num"] == selected_color_num:
+        if selected_color_num != None:
+            print(selected_color_num)
+            message = "{}번 블럭 선택".format(selected_color_num-1)
+            block = block_list[selected_color_num-block_num]
+            select_block_num = selected_color_num-block_num
+    message_surface = font.render(message, True, (0, 0, 0))  # 메시지를 렌더링하여 Surface 생성
+    screen.blit(message_surface, message_pos)  # 메시지 Surface를 화면에 그리기
+
+
+    pygame.display.flip()
+
+
+
     pygame.time.Clock().tick(60)
 
 pygame.quit()
--- test.py
+++ test.py
@@ -1,158 +1,151 @@
-#화면 구현 버전
+from blockset_ai import *
 
-
-#퍼즐 맞추기222
-#https://velog.io/@hancihu/Python-Algorithm-%EC%95%8C%EA%B3%A0%EC%8A%A4%ED%8C%9F-%EA%B2%8C%EC%9E%84%ED%8C%90-%EB%8D%AE%EA%B8%B0-BOARDCOVER
-
-#게임판의 크기 H, W (1≤H, W≤10)
-#블록의 모양을 나타내는 격자의 크기 R, C (1≤R, C≤10)
-
-from collections import deque
-import pygame
-
-
-H = 3
-W = 7
 
 board = [[1,0,0,0,0,0,1],
         [1,0,0,0,0,0,1],
         [1,1,0,0,1,1,1]]
-block = [[(0,0), (0,1), (-1,1)], [(0,0),(1,0),(1,1)], [(0,0),(1,0),(0,1)], [(0,0),(0,1),(1,1)], #ㄴ 모양
+block_list = [[(0,0), (0,1), (-1,1)], [(0,0), (0,1), (0,2)], [(0,0), (0,1), (0,2), (1,0)], [(0,0), (0,1), (0,2), (1,1)], [(0,0), (0,1), (1,0), (1,1)]]
+block = []
+#block = [(0,0), (0,1), (-1,1)]
+'''block = [[(0,0), (0,1), (-1,1)], [(0,0),(1,0),(1,1)], [(0,0),(1,0),(0,1)], [(0,0),(0,1),(1,1)], #ㄴ 모양
          [(0,0), (0,1), (0,2)], [(0,0), (1,0), (2,0)], #일렬, 3칸
          [(0,0), (0,1), (0,2), (1,0)], [(0,0), (0,1), (0,2), (-1,0)], [(0,0), (0,1), (0,2), (1,2)], [(0,0), (0,1), (0,2), (-1,2)], [(0,0), (1,0), (2,0), (0,1)], [(0,0), (1,0), (2,0), (0,-1)], [(0,0), (1,0), (2,0), (2,1)], [(0,0), (1,0), (2,0), (2,-1)], #긴 ㄴ모양
          [(0,0), (0,1), (0,2), (1,1)], [(0,0), (0,1), (0,2), (-1,1)], [(0,0), (1,0), (2,0), (1,1)], [(0,0), (1,0), (2,0), (1,-1)], #ㅗ 모양
-         [(0,0), (0,1), (1,1), (1,2)], [(0,0), (0,1), (-1,1), (-1,2)], [(0,0), (0,-1), (1,-1), (1,-2)], [(0,0), (0,-1), (-1,-1), (-1,-2)], [(0,0), (1,0), (1,1), (2,1)], [(0,0), (1,0), (1,-1), (2,-1)], [(0,0), (-1,0), (-1,1), (-2,1)], [(0,0), (-1,0), (-1,-1), (-2,-1)], #ㄹ모양
-         [(0,0), (0,1), (1,0), (1,1)] #ㅁ모양
-         ]
-#answer = []
+         [(0,0), (0,1), (1,1), (1,2)], [(0,0), (0,1), (-1,1), (-1,2)], [(0,0), (1,0), (1,1), (2,1)], [(0,0), (1,0), (1,-1), (2,-1)], #ㄹ모양
+         [(0,0), (0,1), (1,0), (1,1)], #ㅁ모양
+         [(0,0), (0,1), (0,2), (0,3)], [(0,0), (1,0), (2,0), (3,0)], #일렬,4칸
+         [(0,0), (0,1), (1,1), (2,1), (2,2)], [(0,0), (0,-1), (1,-1), (2,-1), (2,-2)], [(0,0), (1,0), (2,0), (2,1), (2,2)], [(0,0), (-1,0), (-1,1), (-2,1), (-2,2)], #긴 ㄹ모양
+         [(0,0), (0,1), (1,0), (1,1), (2,0)], [(0,0), (0,1), (1,0), (1,1), (2,1)], [(0,0), (0,1), (1,0), (1,1), (-1,0)], [(0,0), (0,1), (1,0), (1,1), (-1,1)], [(0,0), (0,1), (1,0), (1,1), (0,2)], [(0,0), (0,1), (1,0), (1,1), (1,2)], [(0,0), (0,1), (1,0), (1,1), (0,-1)], [(0,0), (0,1), (1,0), (1,1), (1,-1)], #b 모양
+         [(0,0), (0,1), (1,1), (1,2), (1,3)], [(0,0), (0,1), (-1,1), (-1,2), (-1,3)], [(0,0), (0,1), (0,2), (1,2), (1,3)], [(0,0), (0,1), (0,2), (-1,2), (-1,3)], [(0,0), (1,0), (1,1), (2,1), (3,1)], [(0,0), (1,0), (1,-1), (2,-1), (3,-1)], [(0,0), (1,0), (2,0), (2,1), (3,1)], [(0,0), (1,0), (2,0), (2,-1), (3,-1)], #2/3칸 모양
+         [(0,0), (0,1), (1,0), (2,0), (2,1)], [(0,0), (0,-1), (1,0), (2,0), (2,-1)], [(0,0), (1,0), (0,1), (0,2), (1,2)], [(0,0), (-1,0), (0,1), (0,2), (-1,2)], #ㄷ모양
+         [(0,0), (0,1), (0,2), (0,3), (0,4)], [(0,0), (1,0), (2,0), (3,0), (4,0), (5,0)], #일렬, 5칸
+         ]'''
 
+pygame.init()
 
-def draw_board(screen, board):
-    screen.fill((255, 255, 255))  # 화면을 흰색으로 채우기
+H, W = 3, 7
+block_num = 2
+selected_color_num = None
+original_board = [row[:] for row in board]  # 게임 보드의 초기 상태 저장
+original_block = [row[:] for row in block_list]
 
-    block_size = 30  # 각 블록의 크기
-    border_size = 5  # 테두리 크기
+screen = pygame.display.set_mode((W * 50, H * 200))
+pygame.display.set_caption("Block Game")
 
+font = pygame.font.Font('Maplestory Bold.ttf', 36)  # 폰트 설정
+message = ""
+message_pos = (60, 500)  # 메시지 위치 설정 (x, y)
 
-    for y, row in enumerate(board):
-        for x, cell in enumerate(row):
-            rect = pygame.Rect(x * block_size, y * block_size, block_size, block_size)
-            pygame.draw.rect(screen, (255, 255, 255), rect)  # 블록 테두리 그리기
-            inner_rect = pygame.Rect(rect.left + border_size, rect.top + border_size, rect.width - 2 * border_size, rect.height - 2 * border_size)
+def check_block_placement(mouse_x, mouse_y, block_x, block_y):  #해당 위치에 블록을 넣을 수 있는지 판별
+    for dx, dy in block:
+        nx, ny = block_x + dx, block_y + dy
+        if nx < 0 or nx >= W or ny < 0 or ny >= H:
+            return False
+        if board[ny][nx] != 0:
+            return False
+    return True
 
-            if cell == 0:
-                color = (255, 255, 255)     #흰색: 비어있는 블럭
-            elif cell == 1:
-                color = (0, 0, 0)   #검정색: 이미 차있는 블럭
-            elif cell == 2:
-                color = (0, 255, 0)     #초록색(녹색성분)
-            elif cell == 3:
-                color = (0, 128, 0)     #초록색
-            elif cell == 4:
-                color = (255, 0, 0)   #레드(빨강 성분)
-            elif cell == 5:
-                color = (255, 165, 0)   #주황색(빨강, 녹색성분)
-            elif cell == 6:
-                color = (255, 140, 0)     #주황색
-            elif cell == 7:
-                color = (255, 99, 71)   #주황색
-            elif cell == 8:
-                color =  (0, 0, 255)   #파란색
-            elif cell == 9:
-                color = (0, 0, 128)   #파란색
-            elif cell == 10:
-                color = (0, 0, 139)   #파란색
-            elif cell == 11:
-                color = (128, 0, 128)   #보라색(빨강, 파랑 성분)
-            elif cell == 12:
-                color = (148, 0, 211)    #보라색
-            elif cell == 13:
-                color = (255, 255, 0)    #노란색(빨강, 녹색 성분)
-            elif cell == 14:
-                color = (255, 255, 102)    #노란색
-            elif cell == 15:
-                color = (255, 105, 180)    #핑크색 (빨강 성분)
-            elif cell == 15:
-                color = (255, 182, 193)    #핑크색 (빨강 성분)    
+running = True
+hovered_block = None  # 마우스 위에 있는 블록 위치
+color_changed = False
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        elif block!=None and event.type == pygame.MOUSEMOTION:  #마우스 이동 이벤트 발생시 마우스 x, y값 가져옴
+            mouse_x, mouse_y = event.pos
+            block_x = mouse_x // 50
+            block_y = mouse_y // 60
             
-            pygame.draw.rect(screen, color, inner_rect)
+            if hovered_block == None:
+                if check_block_placement(mouse_x, mouse_y, block_x, block_y):   #블록을 현재 위치에 넣을 수 있다면 확인하기
+                    for dx, dy in block:
+                        nx, ny = block_x + dx, block_y + dy
+                        if 0 <= nx < W and 0 <= ny < H:
+                            #original_board[ny][nx] = board[ny][nx]      
+                            board[ny][nx] = selected_color_num   #신규 hover된 위치의 색깔을 바꾸기
+                    color_changed = True
+                    hovered_block = (block_x, block_y)  # 현재 이 블록에 hover했다고 설정한다.
+
+            if hovered_block != None and hovered_block != (block_x, block_y):   #이전 hover된 블록과 현재 hover된 블록이 다르다면
+                for dx, dy in block:
+                    nx, ny = hovered_block[0] + dx, hovered_block[1] + dy
+                    if 0 <= nx < W and 0 <= ny < H:
+                        board[ny][nx] = original_board[ny][nx]      #보드의 상태를 하버되기 전 상태로 되돌리기
+                color_changed = True
             
+                if check_block_placement(mouse_x, mouse_y, block_x, block_y):   #블록을 현재 위치에 넣을 수 있다면 확인하기
+                    for dx, dy in block:
+                        nx, ny = block_x + dx, block_y + dy
+                        if 0 <= nx < W and 0 <= ny < H:
+                            #original_board[ny][nx] = board[ny][nx]      
+                            board[ny][nx] = selected_color_num   #신규 hover된 위치의 색깔을 바꾸기
+                    color_changed = True
+                    hovered_block = (block_x, block_y)  # 현재 이 블록에 hover했다고 설정한다.
+                else:
+                    hovered_block = None
+
+        elif block!=None and event.type == pygame.MOUSEBUTTONDOWN:    #마우스 클릭시
+            if color_changed and hovered_block:     #색상이 변경된 상태이면서 hover한 블록이 있는 경우
+                for dx, dy in block:
+                    nx, ny = hovered_block[0] + dx, hovered_block[1] + dy
+                    if 0 <= nx < W and 0 <= ny < H:
+                        board[ny][nx] = original_board[ny][nx]  #원래 상태로 되돌리기
+                color_changed = False   #색상 변경 상태 초기화
+                hovered_block = None
+            else:
+                mouse_x, mouse_y = event.pos
+                block_x = mouse_x // 50
+                block_y = mouse_y // 60
+                if check_block_placement(mouse_x, mouse_y, block_x, block_y):
+                    for dx, dy in block:
+                        nx, ny = block_x + dx, block_y + dy
+                        if 0 <= nx < W and 0 <= ny < H:
+                            original_board[ny][nx] = board[ny][nx]
+                            board[ny][nx] = selected_color_num
+                    color_changed = True
+                    block = []
+                    message = ""
+                    block_list.remove(block_list[select_block_num])
+                    block_num += 1
+                    '''for block_info in block_info:
+                        if block_info["color_num"] == selected_color_num:
+                            block_list'''
+
+
+
+                    
+
+
+    draw_board(screen, board)
+    block_info = draw_block(screen, block_list, H, block_num)
+
+    
+
+    if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:  # 마우스 왼쪽 버튼 클릭 이벤트일 때
+        mouse_x, mouse_y = event.pos
+        selected_color_num = None
+        for block_info in block_info:
+            if block_info["rect"].collidepoint(mouse_x, mouse_y):
+                selected_color_num = block_info["color_num"]
+                break
+                # selected_color_num에 해당하는 블록들을 찾아 처리
+                #for block_info in block_info:
+                    #if block_info["color_num"] == selected_color_num:
+        if selected_color_num != None:
+            print(selected_color_num)
+            message = "{}번 블럭 선택".format(selected_color_num-1)
+            block = block_list[selected_color_num-block_num]
+            select_block_num = selected_color_num-block_num
+    message_surface = font.render(message, True, (0, 0, 0))  # 메시지를 렌더링하여 Surface 생성
+    screen.blit(message_surface, message_pos)  # 메시지 Surface를 화면에 그리기
+
+
     pygame.display.flip()
 
 
-def boardCover(screen):
-    Q = deque()
-    L = 0
-    Q.append((board, L))
-    
-    count = 0
-    
-    while Q:    #BFS방법 이용하기
-        cur_board, level = Q.popleft()  #큐에서 가장 왼쪽꺼 빼내고
-        
-        x, y = -1, -1
-        for i in range(H):
-            for j in range(W):
-                if cur_board[i][j] == 0:    #board에서 비어있는 구역이라면
-                    x = j
-                    y = i
-                    break   #비어있는 i, j값을 x, y로 저장하기
-            if y != -1:     
-                break   
-                
-        if x == -1 and y == -1:     #모두 다 차있다면
-            count += 1
-            draw_board(screen, cur_board)
-            for i in range(H):
-                for j in range(W):
-                    print(cur_board[i][j], end =' ')
-                print("\n")
-            print()
-            return 1
-            #continue
-        
-        
-        for cover in block:
-            flag = True
-            for dx, dy in cover:
-                nx, ny = x + dx, y + dy
-                if nx < 0 or nx >= W or ny < 0 or ny >= H:   #범위 밖이라면
-                    flag = False
-                    break
-                elif cur_board[ny][nx] != 0:    #이미 그 자리에 차있다면
-                    flag = False
-                    break 
-            if flag:    #넣을 수 있다고 판단했다면
-                new_board = [row[:] for row in cur_board]   #원본 cur_board를 수정하지 않고 복사하고,
-                #a = []
-                for dx, dy in cover:
-                    nx, ny = x + dx, y + dy
-                    new_board[ny][nx] = level+2
-                    #a.append([ny, nx])
-                    #board[ny][nx] = block_num   #채우기
-                    #answer.append([ny, nx])
-                #answer.append(a)
-                #new_board = [''.join(row) for row in new_board]  # 리스트를 문자열로 변환
-                Q.append((new_board, level+1))  #새로운 상태의 보드를 생성하여 큐에 추가하기
-                draw_board(screen, new_board)
-                pygame.time.wait(500)  # 0.5초 대기
-                
-                #if count != 0:
-                #    break
-            #else:
-                #answer.clear()
-        '''
-        a=[]
-        for dx, dy in cover:
-            nx, ny = x + dx, y+ dy
-            a.append([ny, nx])
-        answer.append(a)'''
-    return count
-        
 
-screen = pygame.display.set_mode((W * 30, H * 30))
-boardCover(screen)
-#print(answer)
+    pygame.time.Clock().tick(60)
 
-pygame.time.wait(5000)  # 5초 대기
 pygame.quit()
